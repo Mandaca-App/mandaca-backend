e51b052164b0a20fd1cbe8b735d8cb2296174324
Davi Vieira <62678550+notsogreatdavi@users.noreply.github.com>
2026-04-03 22:10:47 -0300

    SCRUM-84: cria model AudioTranscription com soft delete

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,5 @@
 from app.models.assessment import Assessment as Assessment
+from app.models.audio_transcription import AudioTranscription as AudioTranscription
 from app.models.enterprise import Enterprise as Enterprise
 from app.models.menu import Menu as Menu
 from app.models.photo import Photo as Photo
